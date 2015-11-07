82f153ac31af34978aabbf7009add07f6c710880
ngo.h.gia <ngohgia@u.nus.edu>
2015-11-08 00:11:02 +0800

    Updated Profile screen

--- js/services.js
+++ js/services.js
@@ -10,11 +10,35 @@ angular.module('starter.services', [])
   const users = [
     {
       name: 'Gia',
-      picture: 'img/gia.jpg'
+      picture: 'img/gia.jpg',
+      age: 24,
+      music: [
+        { name: "Jazz", checked: false },
+        { name: "R&B", checked: true },
+        { name: "Rock", checked: false },
+      ],
+      sports: [
+        { name: "Soccer", checked: true},
+        { name: "Basketball", checked: true},
+        { name: "Rugby", checked: false},
+      ],
+      others: 'Travelling, Arts'
     },
     {
       name: 'Tomek',
-      picture: 'img/tomek.jpg'
+      picture: 'img/tomek.jpg',
+      age: 36,
+      music: [
+        { name: "Jazz", checked: true },
+        { name: "R&B", checked: true },
+        { name: "Rock", checked: false },
+      ],
+      sports: [
+        { name: "Soccer", checked: false},
+        { name: "Basketball", checked: false},
+        { name: "Rugby", checked: true},
+      ],
+      others: 'Travelling'
     }
   ]
   return {
